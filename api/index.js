60d4fe1d00ec0deac77d4c3b662b857a7cf237d7
Amirhosein-Bavandpour <bavandpour.amirhosein@gmail.com>
2024-06-28 04:12:43 +0330

    add cat,post,user api and finish api side

--- api/index.js
+++ api/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authRoute = require("./routes/auth")
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer")
 
 dotenv.config();
 app.use(express.json());
@@ -14,7 +18,26 @@ mongoose
   .connect(url)
   .then(() => {
     console.log('Connected to MongoDB');
+
+    const storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+        cb(null, "images");
+      },
+      filename: (req, file, cb) => {
+        cb(null, req.body.name);
+      },
+    });
+
+    const upload = multer({ storage: storage });
+    app.post("/api/upload", upload.single("file"), (req, res) => {
+      res.status(200).json("File has been uploaded.");
+    });
+
     app.use("/api/auth", authRoute);
+    app.use("/api/users", userRoute);
+    app.use("/api/posts", postRoute);
+    app.use("/api/categories", categoryRoute);
+
     app.listen(PORT, () => {
       console.log("Backend is running");
     });
